0c019165771528547511e91861b03f386bdf8512
Andres Mendoza <andres.david.mm@hotmail.com>
2023-09-25 21:54:33 -0500

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const imageRouter = require('./image.router');
 const router = express.Router();
 
 // colocar las rutas aquí
-
+router.use('/images', imageRouter);
 
 module.exports = router;
